1b3f5975187c23bc73c7b8b12e28276b3b1d1c1d
Piyush Mittal <m@piyushmittal.com>
2016-10-24 08:49:39 +0530

    Logo updates

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -3,9 +3,14 @@ angular.module('app.controllers', [])
 .controller('homePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
+<<<<<<< Updated upstream
 function ($scope, $stateParams, StackDataFactory) {
     $scope.getTechGraph = function(){
         console.log(" Get Graph Call ---------- ");
+=======
+function ($scope, $stateParams) {
+    $scope.getTechGraph = function(){
+>>>>>>> Stashed changes
         var n= listnew ;
         // Build the chart
         $('#graphcontainer').highcharts({
