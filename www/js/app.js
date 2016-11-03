8adebadc2ce7a14df6afeaa65f2cb987421eea8f
Piyush Mittal <m@piyushmittal.com>
2016-11-03 08:39:07 +0530

    Ui changes

--- www/js/app.js
+++ www/js/app.js
@@ -5,35 +5,50 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services','ionic.cloud'])
+
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives', 'app.services', 'ionic.cloud'])
 
 .config(function($ionicCloudProvider) {
-  $ionicCloudProvider.init({
-    "core": {
-      "app_id": "2c145d19"
-    }
-  });
+    $ionicCloudProvider.init({
+        "core": {
+            "app_id": "2c145d19"
+        },
+        "push": {
+            "sender_id": "923688165386",
+            "pluginConfig": {
+                "ios": {
+                    "badge": true,
+                    "sound": true
+                },
+                "android": {
+                    "iconColor": "#343434"
+                }
+            }
+        }
+    });
 })
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        console.log(" ________ ");
+        console.log(globalData.app_id);
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-listnew=[{name: 'javascript',y:  37},{name: 'java',y:  30},{name: 'android',y: 24},
-         {name: 'php',y:  23},{name: 'python',y:   22},{name: 'c# ',y: 21},
-         {name: 'html',y:   17},{name: 'jquery',y:   15},{name: 'ios',y: 13},
-         {name: 'css',y:  11},{name: 'c++',y:  10},{name: 'mysql',y: 10},
-         {name: 'angularjs',y:  9},{name: 'swift',y:  8},{name: 'sql',y: 7}
-        
-        ];
+listnew = [{ name: 'javascript', y: 37 }, { name: 'java', y: 30 }, { name: 'android', y: 24 },
+    { name: 'php', y: 23 }, { name: 'python', y: 22 }, { name: 'c# ', y: 21 },
+    { name: 'html', y: 17 }, { name: 'jquery', y: 15 }, { name: 'ios', y: 13 },
+    { name: 'css', y: 11 }, { name: 'c++', y: 10 }, { name: 'mysql', y: 10 },
+    { name: 'angularjs', y: 9 }, { name: 'swift', y: 8 }, { name: 'sql', y: 7 }
+
+];
